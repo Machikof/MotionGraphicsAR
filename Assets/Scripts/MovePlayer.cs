00eef05812efa99254c6a7d79405a3451bbbd7fe
Machinaga Kosuke <machikou@icloud.com>
2021-01-12 00:31:11 +0900

    feat: JoyStickでドラゴンを動かせるようにした

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed;
+    public FloatingJoystick floatingJoystick;
+    public Rigidbody rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        float angle = Mathf.Atan(floatingJoystick.Vertical / floatingJoystick.Horizontal);
+        Debug.Log(angle);
+    }
+}
